44bc5c67c6205f16fe15140ce39319aeb932f70c
user108 <you@example.com>
2017-09-08 15:38:43 +0200

    'validation'

--- jQuery/controllerJQ.js
+++ jQuery/controllerJQ.js
@@ -2,7 +2,7 @@
 Property query van de eerste parameter van de callback functie van methode get stelt de waarden van de doorgestuurde invoervelden voor. (of juister: van de querystring, dit is in de url van een request het gedeelte na het vraagteken, dat bestaat uit key-value paren)
 Om hetzelfde te doen als de form naar de server gestuurd is via een POST, moeten we vooraf module body-parser installeren (npm install body-parser) en gebruiken: */
 
-$(function() {
+$(function () {
 
     var interval;
     var timer;
@@ -11,191 +11,204 @@ $(function() {
 
     $("#divSpel").hide();
 
-    $("#btnStart").click(function() {
+    $("#btnStart").click(function () {
+        if ($('#invoerBommen').val() < 1 || $('#invoerBommen').val() >= $('#invoerRijen').val() * $('#invoerKolommen').val()) {
+            alert('Gelieve het spelidee te respecteren')
+        } else {
 
-        $('#speelveld').remove();
-        $("#divSpel").show();
-        var tellerV = 0;
-        var aantalRijen = $("#invoerRijen").val();
-        var aantalKolommen = $("#invoerKolommen").val();
-        var aantalBommen = $("#invoerBommen").val();
-        document.getElementById("aantalNogTeMarkerenBommen").innerHTML = aantalBommen;
-        var spelersnaam = $("#invoerNaam").val();
+            console.log("bommen: " + $('#invoerBommen').val());
+            console.log("rijen: " + $('#invoerRijen').val());
+            console.log("kolommen: " + $('#invoerKolommen').val());
+            console.log('product: ' + $('#invoerRijen').val() * $('#invoerKolommen').val());
 
-        $('#divSpel').append($("<table>").attr('id', 'speelveld'));
-
-        $("#speelveld").one("mousedown", function() {
-            timer.starten();
-        });
-
-        for (var i = 0; i < aantalRijen; i++) {
-            $("#speelveld").append($('<tr>').attr('id', i))
-            for (var y = 0; y < aantalKolommen; y++) {
-                console.log(i + '.' + y)
-                $('#' + i).append($('<td>').attr('id', i + '_' + y)
-                    .click(function(event) {
-                        var rij = this.id.split('_')[0];
-                        var kolom = this.id.split('_')[1];
-                        console.log("links ", rij, ": ", kolom);
-                        $('#' + this.id).attr('class', 'clicked');
-                        $('#' + this.id);
-                        spel.vakjeOmdraaien(rij, kolom);
-                        grafischeWeergaveAanpassen();
-                        controleerEindeSpel();
-                    }).contextmenu(function(event) {
-                        var rij = this.id.split('_')[0]
-                        var kolom = this.id.split('_')[1]
-                        spel.bord[rij][kolom].vlag()
-                        $("#" + this.id).html(spel.bord[rij][kolom].symboolBepalen())
-                        console.log("rechts ", rij, ": ", kolom);
-                        if (spel.bord[rij][kolom].symboolBepalen() == 'v') {
-                            tellerV++;
-                        } else if (spel.bord[rij][kolom].symboolBepalen() == '?') {
-                            tellerV--;
-                        }
-                        document.getElementById("aantalNogTeMarkerenBommen").innerHTML = aantalBommen - tellerV;
-                    })
-                );
-            }
-        }
-
-        function grafischeWeergaveAanpassen() {
-            // } //else {
-            for (var i = 0; i < spel.bord.length; i++) {
-                for (var y = 0; y < spel.bord[i].length; y++) {
-                    if (spel.bord[i][y].omgedraaid) {
-                        $('#' + i + '_' + y).attr('class', 'gedraaid')
-                    }
-                    // console.log(this.bomBuren)
-                    if (spel.bord[i][y].bomBuren) {
-                        //bomburen opsporen
-                        $('#' + i + '_' + y).html(spel.bord[i][y].bomBuren)
-                        console.log(spel.bord[i][y].bomBuren)
-                    }
-                }
-            }
-            //}
-        }
-
-        var spel = new Spel(spelersnaam, aantalBommen, aantalRijen, aantalKolommen);
-
-        console.log(spel.bord);
-
-        var config = {
-            "bommen": aantalBommen,
-            "rijen": aantalRijen,
-            "kolommen": aantalKolommen
-        }
-
-        bewaarInLocalStorage(config);
-
-        timer = new MijnTimer();
-
-        interval = setInterval(function() {
-            document.getElementById("toonTijd").innerHTML = timer.seconden;
-        }, 1000);
-
-        $("#btnPauzeer").click(function() {
-            timer.stoppen();
-            $("#divSpel").hide();
-        });
-
-        $("#btnHerneem").click(function() {
-            timer.hernemen();
+            $('#speelveld').remove();
             $("#divSpel").show();
-        });
+            var tellerV = 0;
+            var aantalRijen = $("#invoerRijen").val();
+            var aantalKolommen = $("#invoerKolommen").val();
+            var aantalBommen = $("#invoerBommen").val();
+            document.getElementById("aantalNogTeMarkerenBommen").innerHTML = aantalBommen;
+            var spelersnaam = $("#invoerNaam").val();
 
-        function controleerEindeSpel() {
-            console.log(spel.boem);
-            if (spel.win || spel.boem) {
-                timer.stoppen();
-            }
-        }
+            $('#divSpel').append($("<table>").attr('id', 'speelveld'));
 
-        function bewaarInLocalStorage(config) {
-            localStorage.setItem("configuratie", JSON.stringify(config));
-        }
+            $("#speelveld").one("mousedown", function () {
+                timer.starten();
+            });
 
-        $('#getIt').click(function() {
-
-            var naam = $("#dnNaam").val(),
-                bom = $("#dnBom").val(),
-                rij = $("#dnRij").val(),
-                kolom = $("#dnKolom").val();
-
-            $.ajax({
-                url: "http://192.168.23.124:1111/deelnemers",
-                async: true, // overbodig
-                /* success: function (param) {
-                    verwerkGegevens(param)
-                }, */
-                data: {
-                    "naam": naam,
-                    "bommen": bom,
-                    "rijen": rij,
-                    "kolommen": kolom
-                },
-                dataType: 'json'
-            }).done(function(param) {
-                console.log("naam: " + naam)
-                console.log(param)
-                verwerkGegevens(param)
-            })
-        })
-
-
-        function verwerkGegevens(data) {
-            // if ($("#tabelDeelnrs")) {
-            $("#tabelDeelnrs").remove()
-                // }  // test blijkbaar niet nodig...?
-            console.log('data fie is ' + data)
-            var arr = data
-
-            $('#getIt').after(($('<table>').attr('id', 'tabelDeelnrs'))
-                .append($('<thead>')
-                    .append($('<th>').html('Naam'))
-                    .append($('<th>').html('Tijd'))
-                    .append($('<th>').html('Bommen'))
-                    .append($('<th>').html('Rijen'))
-                    .append($('<th>').html('Kolommen')))
-                .append($('<tbody>').attr('id', 'dlns')))
-            console.log(arr)
-            arr.forEach(function(deelnemer) {
-                $('#dlns').append($('<tr>')
-                    .append($('<td>').html(deelnemer.naam))
-                    .append($('<td>').html(deelnemer.tijd))
-                    .append($('<td>').html(deelnemer.bommen))
-                    .append($('<td>').html(deelnemer.rijen))
-                    .append($('<td>').html(deelnemer.kolommen)))
-            }, this);
-        }
-
-        $('#btnSubmit').click(function(e) {
-            console.log("let's go!: " + e)
-            var naam = $("#naam").val(),
-                bom = $("#bom").val(),
-                rij = $("#rij").val(),
-                kolom = $("#kolom").val(),
-                tijd = 0;
-            $.post({
-                url: "http://192.168.23.124:1111/nieuw",
-                data: {
-                    "naam": naam,
-                    "bommen": bom,
-                    "rijen": rij,
-                    "kolommen": kolom,
-                    "tijd": tijd
-                },
-                success: function() {
-                    console.log('ok')
+            for (var i = 0; i < aantalRijen; i++) {
+                $("#speelveld").append($('<tr>').attr('id', i))
+                for (var y = 0; y < aantalKolommen; y++) {
+                    console.log(i + '.' + y)
+                    $('#' + i).append($('<td>').attr('id', i + '_' + y)
+                        .click(function (event) {
+                            var rij = this.id.split('_')[0];
+                            var kolom = this.id.split('_')[1];
+                            console.log("links ", rij, ": ", kolom);
+                            $('#' + this.id).attr('class', 'clicked');
+                            $('#' + this.id);
+                            spel.vakjeOmdraaien(rij, kolom);
+                            grafischeWeergaveAanpassen();
+                            controleerEindeSpel();
+                        }).contextmenu(function (event) {
+                            var rij = this.id.split('_')[0]
+                            var kolom = this.id.split('_')[1]
+                            spel.bord[rij][kolom].vlag()
+                            $("#" + this.id).html(spel.bord[rij][kolom].symboolBepalen())
+                            console.log("rechts ", rij, ": ", kolom);
+                            if (spel.bord[rij][kolom].symboolBepalen() == 'v') {
+                                tellerV++;
+                                $(this).addClass('alert');
+                            } else if (spel.bord[rij][kolom].symboolBepalen() == '?') {
+                                tellerV--;
+                                $(this).addClass('warning');
+                                $(this).remove('alert')
+                            }
+                            $(this).remove('warning')
+                            document.getElementById("aantalNogTeMarkerenBommen").innerHTML = aantalBommen - tellerV;
+                        })
+                    );
                 }
+            }
+
+            function grafischeWeergaveAanpassen() {
+                // } //else {
+                for (var i = 0; i < spel.bord.length; i++) {
+                    for (var y = 0; y < spel.bord[i].length; y++) {
+                        if (spel.bord[i][y].omgedraaid) {
+                            $('#' + i + '_' + y).attr('class', 'gedraaid')
+                        }
+                        // console.log(this.bomBuren)
+                        if (spel.bord[i][y].bomBuren) {
+                            //bomburen opsporen
+                            $('#' + i + '_' + y).html(spel.bord[i][y].bomBuren)
+                            console.log(spel.bord[i][y].bomBuren)
+                        }
+                    }
+                }
+                //}
+            }
+
+            var spel = new Spel(spelersnaam, aantalBommen, aantalRijen, aantalKolommen);
+
+            console.log(spel.bord);
+
+            var config = {
+                "bommen": aantalBommen,
+                "rijen": aantalRijen,
+                "kolommen": aantalKolommen
+            }
+
+            bewaarInLocalStorage(config);
+
+            timer = new MijnTimer();
+
+            interval = setInterval(function () {
+                document.getElementById("toonTijd").innerHTML = timer.seconden;
+            }, 1000);
+
+            $("#btnPauzeer").click(function () {
+                timer.stoppen();
+                $("#divSpel").hide();
+            });
+
+            $("#btnHerneem").click(function () {
+                timer.hernemen();
+                $("#divSpel").show();
+            });
+
+            function controleerEindeSpel() {
+                console.log(spel.boem);
+                if (spel.win || spel.boem) {
+                    timer.stoppen();
+                }
+            }
+
+            function bewaarInLocalStorage(config) {
+                localStorage.setItem("configuratie", JSON.stringify(config));
+            }
+
+            $('#getIt').click(function () {
+
+                var naam = $("#dnNaam").val(),
+                    bom = $("#dnBom").val(),
+                    rij = $("#dnRij").val(),
+                    kolom = $("#dnKolom").val();
+
+                $.ajax({
+                    url: "http://192.168.23.124:1111/deelnemers",
+                    async: true, // overbodig
+                    /* success: function (param) {
+                        verwerkGegevens(param)
+                    }, */
+                    data: {
+                        "naam": naam,
+                        "bommen": bom,
+                        "rijen": rij,
+                        "kolommen": kolom
+                    },
+                    dataType: 'json'
+                }).done(function (param) {
+                    console.log("naam: " + naam)
+                    console.log(param)
+                    verwerkGegevens(param)
+                })
             })
 
-            $("#naam").val("");
-            $("#bom").val("");
-            $("#rij").val("");
-            $("#kolom").val("");
-        })
+
+            function verwerkGegevens(data) {
+                // if ($("#tabelDeelnrs")) {
+                $("#tabelDeelnrs").remove()
+                // }  // test blijkbaar niet nodig...?
+                console.log('data fie is ' + data)
+                var arr = data
+
+                $('#getIt').after(($('<table>').attr('id', 'tabelDeelnrs'))
+                    .append($('<thead>')
+                        .append($('<th>').html('Naam'))
+                        .append($('<th>').html('Tijd'))
+                        .append($('<th>').html('Bommen'))
+                        .append($('<th>').html('Rijen'))
+                        .append($('<th>').html('Kolommen')))
+                    .append($('<tbody>').attr('id', 'dlns')))
+                console.log(arr)
+                arr.forEach(function (deelnemer) {
+                    $('#dlns').append($('<tr>')
+                        .append($('<td>').html(deelnemer.naam))
+                        .append($('<td>').html(deelnemer.tijd))
+                        .append($('<td>').html(deelnemer.bommen))
+                        .append($('<td>').html(deelnemer.rijen))
+                        .append($('<td>').html(deelnemer.kolommen)))
+                }, this);
+            }
+
+            $('#btnSubmit').click(function (e) {
+                console.log("let's go!: " + e)
+                var naam = $("#naam").val(),
+                    bom = $("#bom").val(),
+                    rij = $("#rij").val(),
+                    kolom = $("#kolom").val(),
+                    tijd = 0;
+                $.post({
+                    url: "http://192.168.23.124:1111/nieuw",
+                    data: {
+                        "naam": naam,
+                        "bommen": bom,
+                        "rijen": rij,
+                        "kolommen": kolom,
+                        "tijd": tijd
+                    },
+                    success: function () {
+                        console.log('ok')
+                    }
+                })
+
+                $("#naam").val("");
+                $("#bom").val("");
+                $("#rij").val("");
+                $("#kolom").val("");
+            })
+        }
     })
 });
 
