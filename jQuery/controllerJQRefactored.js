8d7b8bcadba2d59a797add509e637305fbfdd0a9
user108 <you@example.com>
2017-09-12 14:23:53 +0200

    di middag

--- jQuery/controllerJQRefactored.js
+++ jQuery/controllerJQRefactored.js
@@ -1,5 +1,5 @@
 /* $(function () {
-
+    
 }) */
 /* Tip verder werker: NodeJSvoorFEO, p. 13:
 Property query van de eerste parameter van de callback functie van methode get stelt de waarden van de doorgestuurde invoervelden voor. (of juister: van de querystring, dit is in de url van een request het gedeelte na het vraagteken, dat bestaat uit key-value paren)
@@ -10,41 +10,39 @@ $(function () {
     var spel;
     var interval;
     var timer;
+    var tellerV;
+    var spel, bommen, rijen, kolommen;
     // haalUitLocalStorage();
 
     var start = $("#btnStart");
 
-    start.on('click', function () {
-        $("#speelveld").one("mousedown", function () {
-            timer.starten();
-        });
 
+    spelersnaam = $("#invoerNaam").val(),
+        bommen = +$("#invoerBommen").val(),
+        rijen = +$("#invoerRijen").val(),
+        kolommen = +$("#invoerKolommen").val(),
+        tellerV = 0;
 
+    if (1 > bommen || bommen >= rijen * kolommen) {
 
-
-        var spelersnaam = $("#invoerNaam").val(),
-            bommen = +$("#invoerBommen").val(),
-            rijen = +$("#invoerRijen").val(),
-            kolommen = +$("#invoerKolommen").val(),
-            tellerV = 0;
-
-        if (1 > bommen || bommen >= rijen * kolommen) {
-
-            // if (($('#invoerBommen').val() < 1) || $('#invoerRijen').val() * ($('#invoerKolommen').val() <= $('#invoerBommen').val())) {
-            // als variabele wordt opgeroepen ipv $('#xyz') wérkt dit niet door toegekenning(?)
-            alert('Gelieve het spelidee te respecteren')
-        } else {
-            console.log('0-test')
-            spel = new Spel(spelersnaam, bommen, rijen, kolommen);
-            bouwVeld(bommen, rijen, kolommen);
-
-        }
-    })
+        // if (($('#invoerBommen').val() < 1) || $('#invoerRijen').val() * ($('#invoerKolommen').val() <= $('#invoerBommen').val())) {
+        // als variabele wordt opgeroepen ipv $('#xyz') wérkt dit niet door toegekenning(?)
+        alert('Gelieve het spelidee te respecteren')
+    } else {
+        $("#showBombs").html("Resterende bommen: " + bommen);
+        spel = new Spel(spelersnaam, bommen, rijen, kolommen);
+        bouwVeld(bommen, rijen, kolommen);
+        timer = new MijnTimer();
+        bewaarGegevens();
+        // TODO
+        // hier bijzetten
+        $('#btnPauzeer').on('click', pauze(timer));
+        $('#btnHerneem').on('click', herneem());
+    }
 
     function bouwVeld(bommen, rijen, kolommen) {
         $("#spelletje").remove();
         $("#divSpelbord").append($("<table>").attr("id", "spelletje"));
-        console.log('fie bouwVeld');
         for (var i = 0; i < rijen; i++) {
             $('#spelletje').append($('<tr>').attr('id', i));
             for (var y = 0; y < kolommen; y++) {
@@ -54,8 +52,6 @@ $(function () {
     }
 
     function voegTDToe(r, k) {
-        console.log('fie voegTDToe');
-        // console.log(r + ' ' + k);
         $('#' + r).append($('<td>')
             .attr('id', r + "_" + k)
             .on('click', function (e) {
@@ -70,117 +66,94 @@ $(function () {
             })
             .on('contextmenu', function () {
 
+                var rij = this.id.split('_')[0]
+                var kolom = this.id.split('_')[1]
+                spel.bord[rij][kolom].vlag()
+                $("#" + this.id).html(spel.bord[rij][kolom].symboolBepalen())
+                if (spel.bord[rij][kolom].symboolBepalen() == 'v') {
+                    tellerV++;
+                    spel.markedVakjes++;
+                    $(this).addClass('alert');
+                } else if (spel.bord[rij][kolom].symboolBepalen() == '?') {
+                    tellerV--;
+                    spel.markedVakjes--;
+                    $(this).removeClass('alert');
+                    $(this).addClass('warning');
+                } else {
+                    $(this).removeClass('warning');
+                }
+                spel.winControle();
+                controleerEindeSpel();
+
+                resterendeBommenTonen();
             })
         )
     }
 
     function grafischeWeergaveAanpassen() {
-
+        for (var i = 0; i < spel.bord.length; i++) {
+            for (var y = 0; y < spel.bord[i].length; y++) {
+                if (spel.bord[i][y].omgedraaid) {
+                    $('#' + i + '_' + y).attr('class', 'gedraaid')
+                }
+                if (spel.bord[i][y].bomBuren) {
+                    //bomburen opsporen
+                    $('#' + i + '_' + y).html(spel.bord[i][y].bomBuren)
+                }
+            }
+        }
     }
 
     function controleerEindeSpel() {
+        console.log("spel: " + spel);
+        console.log("spel.bomBuren: " + spel.bomBuren);
+        if (spel.win || spel.boem) {
+            console.log("win: " + spel.win);
+            console.log("boem: " + spel.boem);
+            timer.stoppen();
+        }
+    }
+
+    function resterendeBommenTonen() {
+        $("#showBombs").html("Resterende bommen: " + (bommen - tellerV));
+    }
+    function newTimer() {
+        start.on('click', function () {
+            timer = new MijnTimer();
+            interval = setInterval(function () {
+                $("#showTime").html("Verstreken tijd: " + timer.seconden);
+            }, 1000);
+            $("#speelveld").one("mousedown", function () {
+                console.log('timer: ' + timer)
+                timer.starten();
+            });
+        })
+    };
+    function pauze(timer) {
+        console.log("timer: " + timer)
+        timer.stoppen();
+        $("#divSpelbord").prepend($('<img>').attr("src", "Images/pauze.gif"))
+        $("#speelveld").hide()
 
     }
-    //             var tellerV = 0;
+    function herneem() {
 
-    //             var spel = new Spel(spelersnaam, aantalBommen, aantalRijen, aantalKolommen);
-
-    //             $("#showBombs").html("Resterende bommen: " + aantalBommen);
-
-    //             $('#divSpelbord').append($("<table>").attr('id', 'speelveld'));
-
-
-    // for (var i = 0; i < aantalRijen; i++) {
-    //     $("#speelveld").append($('<tr>').attr('id', i))
-    //     for (var y = 0; y < aantalKolommen; y++) {
-    //         // console.log(i + '.' + y)
-    //         $('#' + i).append($('<td>').attr('id', i + '_' + y)
-    //             .click(function (event) {
-    //                 var rij = +this.id.split('_')[0];
-    //                 var kolom = +this.id.split('_')[1];
-    //                 // console.log("links ", rij, ": ", kolom);
-    //                 if (spel.bord[rij][kolom].symboolBepalen() != 'v') {
-    //                     $('#' + this.id).attr('class', 'clicked');
-    //                     $('#' + this.id);
-    //                     spel.vakjeOmdraaien(rij, kolom);
-    //                     grafischeWeergaveAanpassen();
-    //                     controleerEindeSpel();
-    //                 }
-    //             }).contextmenu(function (event) {
-    //                 var rij = this.id.split('_')[0]
-    //                 var kolom = this.id.split('_')[1]
-    //                 spel.bord[rij][kolom].vlag()
-    //                 $("#" + this.id).html(spel.bord[rij][kolom].symboolBepalen())
-    //                 // console.log("rechts ", rij, ": ", kolom);
-    //                 if (spel.bord[rij][kolom].symboolBepalen() == 'v') {
-    //                     tellerV++;
-    //                     spel.markedVakjes++;
-    //                     spel.winControle();
-    //                     controleerEindeSpel();
-    //                     $(this).addClass('alert');
-    //                     // $(this).on('click', function () { prop("disabled", false) });
-    //                     // $(this).prop('click()', 'disabled')
-    //                     $(this).attr({ disabled: true })
-    //                     // $(this).attr('disabled', 'disabled')
-    //                     // console.log(this)
-    //                     // console.log($(this))
-    //                 } else if (spel.bord[rij][kolom].symboolBepalen() == '?') {
-    //                     tellerV--;
-    //                     spel.markedVakjes--;
-    //                     spel.winControle();
-    //                     controleerEindeSpel();
-    //                     $(this).removeClass('alert');
-    //                     $(this).addClass('warning');
-    //                     $(this).prop('disabled', true)
-    //                 } else {
-    //                     $(this).removeClass('warning');
-    //                 }
-    //                 $("#showBombs").html("Resterende bommen: " + (aantalBommen - tellerV));
-    //             })
-    //                     );
-    //                 }
-    //             }
-
-    //             function grafischeWeergaveAanpassen() {
-    //                 // } //else {
-    //                 for (var i = 0; i < spel.bord.length; i++) {
-    //                     for (var y = 0; y < spel.bord[i].length; y++) {
-    //                         if (spel.bord[i][y].omgedraaid) {
-    //                             $('#' + i + '_' + y).attr('class', 'gedraaid')
-    //                         }
-    //                         // console.log(this.bomBuren)
-    //                         if (spel.bord[i][y].bomBuren) {
-    //                             //bomburen opsporen
-    //                             $('#' + i + '_' + y).html(spel.bord[i][y].bomBuren)
-    //                             console.log(spel.bord[i][y].bomBuren)
-    //                         }
-    //                     }
-    //                 }
-    //                 //}
-    //             }
+    }
 
     //             console.log(spel.bord);
-
-    //             var config = {
-    //                 "bommen": aantalBommen,
-    //                 "rijen": aantalRijen,
-    //                 "kolommen": aantalKolommen
-    //             }
-
-    //             bewaarInLocalStorage(config);
-
+    function bewaarGegevens() {
+        var config = {
+            "bommen": bommen,
+            "rijen": rijen,
+            "kolommen": kolommen
+        }
+        // bewaarInLocalStorage(config);
+        localStorage.setItem("configuratie", JSON.stringify(config));
+    }
     //             timer = new MijnTimer();
 
-    //             interval = setInterval(function () {
-    //                 document.getElementById("showTime").innerHTML = "Verstreken tijd: " + timer.seconden;
-    //             }, 1000);
 
     //             $("#btnPauzeer").click(function () {
-    //                 timer.stoppen();
-    //                 // $("#divSpelbord").hide();
-    //                 // $("#divSpelbord").addClass('pauze')
-    //                 $("#divSpelbord").prepend($('<img>').attr("src", "Images/pauze.gif"))
-    //                 $("#speelveld").hide()
     //             });
 
 
